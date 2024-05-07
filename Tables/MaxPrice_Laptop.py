e8761748d6d51ffcbc0867690486a9a61934b4be
arroyoruizj <arroyoruizj@carleton.edu>
2024-05-06 22:20:52 -0500

    Finds the highest price laptop

---
+++ Tables/MaxPrice_Laptop.py
@@ -0,0 +1,30 @@
+# MaxPrice_Laptop.py
+# Authors: THe Blind Ones
+
+# This following program was designed in order to return
+# the laptop with the highest price.
+
+import psycopg2
+
+
+def maxPrice():
+
+    # Establishing Environment
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+
+    cur = conn.cursor()
+
+    max_finder = "SELECT companyName, typeName FROM laptops WHERE priceInUSDollars = (SELECT MAX(priceInUSDollars) FROM laptops);"
+
+    cur.execute(max_finder)
+
+    row = cur.fetchall()[0]
+
+    return row
+
+print(maxPrice())
