e81a69683c4c335e3039c5ded268cacbfd9ce124
Mason Moses <masonmoses@Masons-MacBook-Air.local>
2024-05-07 01:25:57 -0500

    changed newcreatetable

--- Tables/newCreateTables.sql
+++ Tables/newCreateTables.sql
@@ -1,17 +1,20 @@
 DROP TABLE IF EXISTS laptops cascade;
 
     CREATE TABLE laptops (
-    Laptop_Name varchar(150) not null,
+    index int not null,
     Brand text not null,
-    Model varchar(15) not null,
+    Laptop_Name varchar(150) not null,
+    Price real not null,
+    Processor_Brand text not null,
     CPU varchar(30) not null,
     RAM int not null,
-    Storage int not null,
     Storage_Type text not null,
+    Storage int not null,
     GPU varchar(15) not null,
+    GPU_Type text not null,
     Screen_Size real not null,
     Touchscreen text not null,
-    Price real not null
+    OS text not null
     );
 
 \copy laptops from 'laptopData.csv' DELIMITER ',' CSV
