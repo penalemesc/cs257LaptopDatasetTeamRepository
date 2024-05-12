5d027a4326e6bac3d5885410d5394094b17da8f0
arroyoruizj <arroyoruizj@carleton.edu>
2024-05-12 15:59:56 -0500

    Starting front end

---
+++ Website/base.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("homepage.html")
+
+if __name__ == '__main__':
+    my_port = 5112
+    app.run(host='0.0.0.0', port = my_port) 
