163e13e75edbd2650f89b5ebb59077adaa8cb44f
Joshua Thompson <joshuaandrewt@outlook.com>
2024-05-24 10:09:22 -0500

    add subrepo with updated design

---
+++ subrepo/Main-Website/mainWeb.py
@@ -0,0 +1,143 @@
+from flask import Flask, render_template, jsonify
+import psycopg2
+import json
+
+
+
+app = Flask(__name__)
+
+
+#Welcome Page
+@app.route('/')
+def welcome():
+    return render_template("mainWebsite.html")
+
+@app.route('/about-us')
+def aboutUsPage():
+    return render_template("aboutUs.html")
+
+#Displays the filtered options results
+@app.route('/display/<brand>/<ram>/<storage>')
+def displayLaptopChosen(brand,ram,storage):
+    return render_template("filterOutput.html")
+
+#Gathers the result for filtered options and return the JSON data
+@app.route('/json/<brand>/<ram>/<storage>')
+def laptopBrandChosen(brand, ram, storage):
+    # Establishing Environment
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="arizavarar",
+        user="arizavarar",
+        password="expo795beach"
+    )
+    
+    cur = conn.cursor()
+    intRam = int(ram)
+    intStor = int(storage)
+    
+    query = '''
+            SELECT Laptop_Name, Price, CPU, RAM, 
+            Screen_Size, Touchscreen, laptopindex FROM laptops 
+            WHERE Brand = %s 
+            AND RAM = %s 
+            AND Storage = %s;
+            '''
+    
+    cur.execute(query, (brand, intRam, intStor))
+
+    rows = cur.fetchall()
+    
+    if not rows:
+        cur.close()
+        conn.close()
+        return jsonify(message="No laptops Matched Your Specifications")
+    
+    laptopsName = [row[0] for row in rows]
+    laptopsPrices = [row[1] for row in rows]
+    laptop_CPU = [row[2] for row in rows]
+    laptop_RAM = [row[3] for row in rows]
+    laptop_screensize =  [row[4] for row in rows]
+    laptop_touchscreen =  [row[5] for row in rows]
+    laptopImages = [row[6] for row in rows]
+    
+    
+    cur.close()
+    conn.close()
+    
+    json_answer = {'nameForLaptop' : laptopsName, 
+                   'priceForLaptop' : laptopsPrices,
+                   'CpuForLaptop' : laptop_CPU, 
+                   'RamForLaptop' : laptop_RAM,
+                   'screensizeLaptop' : laptop_screensize, 
+                   'touchscreenLaptop' : laptop_touchscreen, 
+                   'imageIndex': laptopImages}
+                   
+    return json.dumps(json_answer)
+
+
+#Displays the search result page
+@app.route('/display/<wordSearched>')
+def displaySearched(wordSearched):
+    return render_template("searchOutput.html")
+
+#Gather the searched word content and returns JSON data
+@app.route('/search/<wordSearched>')
+def searchFunction(wordSearched):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="arizavarar",
+        user="arizavarar",
+        password="expo795beach"
+    )
+    
+    cur = conn.cursor()
+
+    query = '''
+            SELECT Laptop_Name, Price, CPU, RAM, 
+            Screen_Size, Touchscreen, laptopindex FROM laptops WHERE 
+            Brand LIKE %s OR 
+            Laptop_Name LIKE %s OR 
+            CAST(Price AS TEXT) LIKE %s OR 
+            Processor_Brand LIKE %s OR 
+            GPU LIKE %s OR 
+            OS LIKE %s;
+            '''
+    
+    search_pattern = '%' + wordSearched + '%'
+    cur.execute(query, (search_pattern, search_pattern, search_pattern, search_pattern, search_pattern, search_pattern))
+
+    rows = cur.fetchall()
+
+    if not rows:
+        cur.close()
+        conn.close()
+        return jsonify(message="No laptops Matched Your Specifications")
+
+    laptopsName = [row[0] for row in rows]
+    laptopsPrices = [row[1] for row in rows]
+    laptop_CPU = [row[2] for row in rows]
+    laptop_RAM = [row[3] for row in rows]
+    laptop_screensize =  [row[4] for row in rows]
+    laptop_touchscreen =  [row[5] for row in rows]
+    laptopImages = [row[6] for row in rows]
+    
+    cur.close()
+    conn.close()
+
+    json_answer = {'nameForLaptop' : laptopsName, 
+                   'priceForLaptop' : laptopsPrices,
+                   'CpuForLaptop' : laptop_CPU, 
+                   'RamForLaptop' : laptop_RAM,
+                   'screensizeLaptop' : laptop_screensize, 
+                   'touchscreenLaptop' : laptop_touchscreen, 
+                   'imageIndex': laptopImages}
+    
+    return json.dumps(json_answer)
+
+if __name__ == '__main__':
+    my_port = 5111
+    app.run(host='0.0.0.0', port=my_port)
+
---
+++ subrepo/Tables/MaxPrice_Laptop.py
@@ -0,0 +1,33 @@
+# MaxPrice_Laptop.py
+# Authors: THe Blind Ones
+
+# This following program was designed in order to return
+# the laptop with the highest price.
+
+import psycopg2
+
+
+def maxPrice():
+
+    # Establishing Environment
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="mosesm2",
+        user="mosesm2",
+        password="field599farm")
+
+    cur = conn.cursor()
+
+    max_finder = "SELECT Brand, Laptop_Name, Price FROM laptops WHERE Price = (SELECT MAX(Price) FROM laptops);"
+
+    cur.execute(max_finder)
+    row = cur.fetchall()[0]
+
+    type_row = row[1]
+    price_row = row[2]
+
+    print("The most expensive laptop is the " +  str(type_row))
+    print("It costs $" + str(price_row) + "!")
+
+maxPrice()
