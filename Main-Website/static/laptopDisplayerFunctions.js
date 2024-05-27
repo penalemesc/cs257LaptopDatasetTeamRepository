6bb1a742f5a66bb332a329a68009efbafac6d3a1
jthompson05 <joshuaandrewt@outlook.com>
2024-05-27 10:41:09 -0500

    update price label

--- Main-Website/static/laptopDisplayerFunctions.js
+++ Main-Website/static/laptopDisplayerFunctions.js
@@ -81,8 +81,8 @@ function generateDisplay(data) {
 
         info.innerHTML =  `
                         <strong>Price of Laptop:</strong> <strong>$</strong>${laptopPrices[i]} <br>
-                        <strong>Storage:</strong> ${laptopStorage[i]} <br>
-                        <strong>Ram:</strong> ${laptop_RAM[i]} <strong>GB</strong> <br>
+                        <strong>Storage:</strong> ${laptopStorage[i]}<strong>GB</strong> <br>
+                        <strong>Ram:</strong> ${laptop_RAM[i]}<strong>GB</strong> <br>
                           `;
 
         profileDetails.appendChild(title);
